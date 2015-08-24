d752a686748887ad0989684bf92bb326987751c2
guilhermemilreu <guilherme.milreu@gmail.com>
2015-08-24 17:34:22 -0300

    Incluida classe Cronometro

---
+++ app/src/main/java/android/milreuelima/iesb/com/br/futebas/entidades/Cronometro.java
@@ -0,0 +1,12 @@
+package android.milreuelima.iesb.com.br.futebas.entidades;
+
+/**
+ * Created by guilhermemilreu on 24/08/15.
+ */
+public class Cronometro {
+
+    // Vaiii corinthians !!!!!
+
+
+    private int variavel;
+}
--- app/src/main/java/android/milreuelima/iesb/com/br/futebas/entidades/Placar.java
+++ app/src/main/java/android/milreuelima/iesb/com/br/futebas/entidades/Placar.java
@@ -4,4 +4,7 @@ package android.milreuelima.iesb.com.br.futebas.entidades;
  * Created by Sinvas on 24/08/2015.
  */
 public class Placar {
+
+
+    // FOIIII??
 }
