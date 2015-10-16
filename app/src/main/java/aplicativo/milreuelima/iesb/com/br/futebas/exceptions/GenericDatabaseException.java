615d9e00acd61fc230ddcd308439dfa4716d0cbb
Sinval Bezerra de Lima <sinval.lima@gmail.com>
2015-10-16 00:05:59 -0300

    Persistência:
    Recupera eveno em andamento;
    Recupera Partida em Andamento;
    Persiste partida a cada evento (gol, mudança de estado, etc)

---
+++ app/src/main/java/aplicativo/milreuelima/iesb/com/br/futebas/exceptions/GenericDatabaseException.java
@@ -0,0 +1,10 @@
+package aplicativo.milreuelima.iesb.com.br.futebas.exceptions;
+
+/**
+ * Criado por Sinvas em 14/10/2015.
+ */
+public class GenericDatabaseException extends Exception {
+    public GenericDatabaseException(String message){
+        super(message);
+    }
+}
